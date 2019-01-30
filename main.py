302a841a0855194f0842ed88ef4d664de0dbf277
vivekpatel99 <vivpatel99@gmail.com>
2019-01-30 17:42:35 +0100

    coordinate transform is done but have not tested

--- main.py
+++ main.py
@@ -59,11 +59,11 @@ def main():
     """
 
     """
-    pwm_jf1 = pwm.PWM(gpio_path=const.JF1_MIO13_919, servo_cal_info=servo_calib.servo_1)
-    pwm_jf4 = pwm.PWM(gpio_path=const.JF4_MIO12_918, servo_cal_info=servo_calib.servo_2)
-    pwm_jf7 = pwm.PWM(gpio_path=const.JF7_MIO0_906, servo_cal_info=servo_calib.servo_3)
-    pwm_jf8 = pwm.PWM(gpio_path=const.JF8_MIO09_915, servo_cal_info=servo_calib.servo_4)
-    pwm_jf9 = pwm.PWM(gpio_path=const.JF9_MIO14_920, servo_cal_info=servo_calib.servo_5)
+    # pwm_jf1 = pwm.PWM(gpio_path=const.JF1_MIO13_919, servo_cal_info=servo_calib.servo_1)
+    # pwm_jf4 = pwm.PWM(gpio_path=const.JF4_MIO12_918, servo_cal_info=servo_calib.servo_2)
+    # pwm_jf7 = pwm.PWM(gpio_path=const.JF7_MIO0_906, servo_cal_info=servo_calib.servo_3)
+    # pwm_jf8 = pwm.PWM(gpio_path=const.JF8_MIO09_915, servo_cal_info=servo_calib.servo_4)
+    # pwm_jf9 = pwm.PWM(gpio_path=const.JF9_MIO14_920, servo_cal_info=servo_calib.servo_5)
 
     end_eff_direction_mat = np.matrix([
         [-1., 0., 0.],
@@ -84,20 +84,20 @@ def main():
         print("theta_4 {}".format(math.degrees(thetas.theta_4)))
         print("theta_5 {}".format(math.degrees(thetas.theta_5)))
 
-        pwm_jf1.pwm_generate(thetas.theta_1)
-        time.sleep(0.5)
-
-        pwm_jf4.pwm_generate(thetas.theta_2)
-        time.sleep(0.5)
-
-        pwm_jf7.pwm_generate(thetas.theta_3)
-        time.sleep(0.5)
-
-        pwm_jf8.pwm_generate(thetas.theta_4)
-        time.sleep(0.5)
-
-        pwm_jf9.pwm_generate(thetas.theta_5)
-        time.sleep(0.5)
+        # pwm_jf1.pwm_generate(thetas.theta_1)
+        # time.sleep(0.5)
+        #
+        # pwm_jf4.pwm_generate(thetas.theta_2)
+        # time.sleep(0.5)
+        #
+        # pwm_jf7.pwm_generate(thetas.theta_3)
+        # time.sleep(0.5)
+        #
+        # pwm_jf8.pwm_generate(thetas.theta_4)
+        # time.sleep(0.5)
+        #
+        # pwm_jf9.pwm_generate(thetas.theta_5)
+        # time.sleep(0.5)
 
 
 if __name__ == '__main__':
